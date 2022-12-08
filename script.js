81ce4a4f1fc1059e985bcb88533876f42c1c17b2
pie999 <pietro.vergara@libero.it>
2022-12-08 10:36:34 +0100

    Change constructor to class

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const library = document.querySelector(".library");
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary() {
